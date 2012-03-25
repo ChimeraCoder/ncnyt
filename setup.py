1477ec349357d5fb2451bd8467d911aae9ad7a35
Zhehao Mao <zhehao.mao@gmail.com>
2012-03-25 08:18:01 -0400

    fix formatting in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='ncnyt',
-      version='0.1',
-      url='https://github.com/ChimeraCoder/ncnyt',
-      packages=['ncnyt', 'nyt'],
-      install_requires=['requests', 'urwid', 'beautifulsoup4', 'lxml'],
-      entry_points = {
-        'console_scripts': ['nyt=ncnyt:main']
-      })
+setup(
+    name='ncnyt',
+    version='0.1',
+    url='https://github.com/ChimeraCoder/ncnyt',
+    packages=['ncnyt', 'nyt'],
+    install_requires=['requests', 'urwid', 'beautifulsoup4', 'lxml'],
+    entry_points = {
+        'console_scripts': ['nyt=ncnyt:main']})
 
 
