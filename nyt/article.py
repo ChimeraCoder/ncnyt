4adb7c7cd87cc5963d2e86d838f996d38bcbf6ce
Zack Newman <znewman01@gmail.com>
2012-03-25 03:40:20 -0400

    added support for multipage articles...ugly but functional

--- nyt/article.py
+++ nyt/article.py
@@ -10,6 +10,20 @@ def extract_text(elem):
         return u' '.join([extract_text(subelem) for subelem in elem.contents])
     return u''
 
+def get_page(url, page):
+    r = requests.get(url, params={'pagewanted': page})
+    if r.status_code != 200:
+        r.raise_for_status()
+
+    soup = bs(r.text, 'lxml')
+
+    paragraphs = soup.find_all('p', {'itemprop':'articleBody'})
+    
+    if paragraphs:
+        return u'\n'.join([extract_text(elem).rstrip() for elem in paragraphs])
+    return u'Could not extract text for page' + str(page)
+
+
 def get_text(url):
     """Given the url of a NYTimes article, returns the body text of
     the article."""
@@ -20,9 +34,22 @@ def get_text(url):
 
     soup = bs(r.text, 'lxml')
 
-    paragraphs = soup.find_all('p', {'itemprop':'articleBody'})
+    pages = soup.find('ul', {'id': 'pageNumbers'})
+
+    if not pages:
+        paragraphs = soup.find_all('p', {'itemprop':'articleBody'})
     
-    if paragraphs:
-        return u'\n'.join([extract_text(elem).rstrip() for elem in paragraphs])
+        if paragraphs:
+            return u'\n'.join([extract_text(elem).rstrip() for elem in paragraphs])
+    else:
+        num_pages = len(pages.find_all('li'))
+        paragraphs = soup.find_all('p', {'itemprop':'articleBody'})
     
-    return 'Could not extract article text'
+        if paragraphs:
+            content = u'\n'.join([extract_text(elem).rstrip() for elem in paragraphs])
+        else:
+            content = u'Could not extract text for page 1\n'
+        for x in range(1, num_pages):
+            content += get_page(url, x + 1) + u'\n'
+        return content
+    return u'Could not extract article text'
