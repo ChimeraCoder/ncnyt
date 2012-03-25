58e8d014f146e951bf44ef3edbe838e5ce6cfde9
Zhehao Mao <zhehao.mao@gmail.com>
2012-03-24 22:08:44 -0400

    adding UI with dummy data

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='ncnyt',
+      version='0.1',
+      url='https://github.com/ChimeraCoder/ncnyt',
+      packages=['ncnyt', 'nyt'],
+      install_requires=['requests', 'urwid'],
+      entry_points = {
+        'console_scripts': ['nyt=ncnyt:main']
+      })
+
+
