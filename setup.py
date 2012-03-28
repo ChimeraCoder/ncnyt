23ecdeeb0fc09ad989204bd4feac6783becf4a71
Zhehao Mao <zhehao.mao@gmail.com>
2012-03-28 12:28:32 -0400

    bump setup.py number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='ncnyt',
-    version='0.1',
+    version='0.2',
     url='https://github.com/ChimeraCoder/ncnyt',
     packages=['ncnyt', 'nyt'],
     install_requires=[
