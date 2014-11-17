d2b0a738bcf54053231fba922230b996ccaec5a3
Low Kian Seong <kianseong@gmail.com>
2014-11-17 11:22:36 +0800

    Update setup.py
    Adding in a `description` to not let project show `UNKNOWN` on `pypi`

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ except ImportError:
 setup(
     name='ncnyt',
     version='0.3.0',
+    description='All the news that\'s fit to print()'
     url='https://github.com/ChimeraCoder/ncnyt',
     packages=['ncnyt', 'nyt'],
     install_requires=[
